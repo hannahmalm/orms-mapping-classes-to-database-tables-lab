9afc786524ae7a63a6dd675285e8eece311fafd9
Hannah Malm <hannahemalm616@gmail.com>
2021-08-18 19:41:31 -0400

    commit tree 32088602f6c230a80bb757997bd9943142937b42

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,35 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+  #reader means that it cannot change
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initalize(name, grade, id=nil)
+    #id is optional because it will be created automaically
+    @id = id
+    @name = name
+    @grade = grade 
+  end 
+
   
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end 
+
+  def save
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
+  SQL 
+
+  DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save 
+    student
+  end 
+
 end
